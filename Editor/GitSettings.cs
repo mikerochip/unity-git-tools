4b7f9b803667b741cc0b11bf3e5dcb43a2cadd93
Mike Schweitzer <mschweitzer@gmail.com>
2023-09-04 01:31:43 -0700

    Apply .editorconfig to GitSettings.cs

--- Editor/GitSettings.cs
+++ Editor/GitSettings.cs
@@ -30,7 +30,7 @@ namespace MikeSchweitzer.Git.Editor
             }
         }
         public static bool HasUsername => !string.IsNullOrWhiteSpace(Username);
-        
+
         // in-memory configuration data
         public static int MaxLockItems { get; set; } = 50;
 
@@ -39,7 +39,7 @@ namespace MikeSchweitzer.Git.Editor
         public static string Branch => instance._branch;
         public static bool HasLfsProcess => instance._LfsProcessExists;
         public static bool HasLfsInRepo => instance._hasLfsConfig;
-        
+
         // locks data
         public static IEnumerable<LfsLock> Locks => instance._Locks;
         public static LfsLockSortType LockSortType => instance._LockSortType;
@@ -50,17 +50,17 @@ namespace MikeSchweitzer.Git.Editor
         public static event Action LocksRefreshed;
         public static event Action<LfsLock> LockStatusChanged;
         #endregion
-        
+
         #region Private Properties
         // example "git lfs locks" result for cloud service (e.g. GitHub.com) and
         // self-hosted service, respectively
         // Assets/foo.png   	username                	ID:123456
         // Assets/foobar.png	Foo Bar (fbar@example.com)	ID:123456
         private static Regex LocksResultRegex { get; } = new (
-            @"^(?<path>.+)\b\s*\t(.+\((?<user>\S+)@\S+\)|(?<user>\S+))\s*\tID:(?<id>\S+)$", 
+            @"^(?<path>.+)\b\s*\t(.+\((?<user>\S+)@\S+\)|(?<user>\S+))\s*\tID:(?<id>\S+)$",
             RegexOptions.Compiled);
         #endregion
-        
+
         #region Private Fields
         private const double UpdateInterval = 30.0f;
         private const int ProcessTimeoutMs = 30_000;
@@ -71,7 +71,7 @@ namespace MikeSchweitzer.Git.Editor
         [SerializeField] private List<LfsLock> _Locks = new();
         [SerializeField] private LfsLockSortType _LockSortType;
         [SerializeField] private bool _LockSortAscending;
-        
+
         private string _repoRootPath;
         private string _branch;
         private bool _hasLfsConfig;
@@ -80,7 +80,7 @@ namespace MikeSchweitzer.Git.Editor
         private readonly ConcurrentDictionary<int, Task<ProcessResult>> _tasks = new();
         private readonly ConcurrentDictionary<int, Task<ProcessResult>> _refreshLocksTasks = new();
         #endregion
-        
+
         #region Public Methods
         public static void Lock(string assetPath)
         {
@@ -96,7 +96,7 @@ namespace MikeSchweitzer.Git.Editor
         {
             instance.UnlockImpl(id, true);
         }
-        
+
         public static void RefreshLocks()
         {
             instance.RefreshLocksImpl();
@@ -105,18 +105,18 @@ namespace MikeSchweitzer.Git.Editor
         public static void ForceRefreshLocks()
         {
             const string title = "Refresh Git Locks";
-            
+
             EditorUtility.DisplayProgressBar(title, "Clearing cache", 0.0f);
             instance._Locks.Clear();
             LocksRefreshed?.Invoke();
-            
+
             EditorUtility.DisplayProgressBar(title, "Waiting for Git tasks", 0.3f);
             instance.WaitForTasks();
-            
+
             EditorUtility.DisplayProgressBar(title, "Requesting locks", 0.6f);
             var result = instance.InvokeLfs("locks");
             instance.ProcessLocksResult(result);
-            
+
             EditorUtility.ClearProgressBar();
         }
 
@@ -127,15 +127,15 @@ namespace MikeSchweitzer.Git.Editor
             instance.SortLocksImpl();
         }
         #endregion
-        
+
         #region Unity Methods
         private void OnEnable()
         {
             _stopAutoRefreshLocks = false;
-            
+
             LoadRepoInfo();
             CacheLfsProcess();
-            
+
             EditorApplication.update += OnUpdate;
             EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
             EditorApplication.quitting += OnQuitting;
@@ -158,7 +158,7 @@ namespace MikeSchweitzer.Git.Editor
             _lastUpdateTime = now;
 
             LoadBranch();
-            
+
             if (!AreLocksRefreshing && !_stopAutoRefreshLocks)
                 RefreshLocksImpl();
         }
@@ -167,7 +167,7 @@ namespace MikeSchweitzer.Git.Editor
         {
             WaitForTasks();
         }
-        
+
         private void OnQuitting()
         {
             WaitForTasks();
@@ -177,7 +177,7 @@ namespace MikeSchweitzer.Git.Editor
         {
             WaitForTasks();
         }
-        
+
         private void OnFocusChanged(bool focused)
         {
             if (!focused)
@@ -185,11 +185,11 @@ namespace MikeSchweitzer.Git.Editor
 
             LoadRepoInfo();
             CacheLfsProcess();
-            
+
             if (!AreLocksRefreshing && !_stopAutoRefreshLocks)
                 RefreshLocksImpl();
         }
-        
+
         // see http://answers.unity.com/answers/1886639/view.html
         private static Action<bool> UnityEditorFocusChanged
         {
@@ -207,7 +207,7 @@ namespace MikeSchweitzer.Git.Editor
             }
         }
         #endregion
-        
+
         #region Helpers
         private static void Save()
         {
@@ -218,15 +218,15 @@ namespace MikeSchweitzer.Git.Editor
         {
             var tasks = new Task[_tasks.Count + _refreshLocksTasks.Count];
             var i = 0;
-            
+
             foreach (var task in _tasks.Values)
                 tasks[i++] = task;
             _tasks.Clear();
-            
+
             foreach (var task in _refreshLocksTasks.Values)
                 tasks[i++] = task;
             _refreshLocksTasks.Clear();
-            
+
             Task.WaitAll(tasks);
         }
         #endregion
@@ -234,12 +234,12 @@ namespace MikeSchweitzer.Git.Editor
         #region Core
         private void LoadRepoInfo()
         {
-            CacheRepoRootDir();
+            CacheRepoPaths();
             LoadBranch();
             LoadLfsConfig();
         }
-        
-        private void CacheRepoRootDir()
+
+        private void CacheRepoPaths()
         {
             if (!string.IsNullOrEmpty(_repoRootPath))
             {
@@ -248,7 +248,7 @@ namespace MikeSchweitzer.Git.Editor
                     _repoRootPath = string.Empty;
                 return;
             }
-            
+
             var directory = Directory.GetParent(Application.dataPath);
             while (directory is { Exists: true })
             {
@@ -262,29 +262,29 @@ namespace MikeSchweitzer.Git.Editor
                 directory = directory.Parent;
             }
         }
-        
+
         private void LoadBranch()
         {
             _branch = string.Empty;
-            
+
             if (!IsGitRepo)
                 return;
-            
+
             var headPath = Path.Combine(_repoRootPath, ".git", "HEAD");
             if (!File.Exists(headPath))
                 throw new Exception($"[Git] Failed to load Git branch from \"{headPath}\"");
-            
+
             var line = File.ReadLines(headPath).First();
             _branch = line.Replace("ref: refs/heads/", "");
         }
-        
+
         private void LoadLfsConfig()
         {
             _hasLfsConfig = false;
-            
+
             if (!IsGitRepo)
                 return;
-            
+
             var configPath = Path.Combine(_repoRootPath, ".git", "config");
             if (!File.Exists(configPath))
                 throw new Exception($"[Git] Failed to load Git config \"{configPath}\"");
@@ -299,13 +299,13 @@ namespace MikeSchweitzer.Git.Editor
             _hasLfsConfig = text.Contains("[lfs");
         }
         #endregion
-        
+
         #region LFS
         private void LockImpl(string path)
         {
             if (Locks.Any(lfsLock => lfsLock._Path == path))
                 return;
-            
+
             var lfsLock = new LfsLock
             {
                 _Path = path,
@@ -315,7 +315,7 @@ namespace MikeSchweitzer.Git.Editor
             };
             _Locks.Add(lfsLock);
             LockStatusChanged?.Invoke(lfsLock);
-            
+
             var task = Task.Run(() => InvokeLfs($"lock \"{path}\""));
             _tasks[task.Id] = task;
 
@@ -327,16 +327,16 @@ namespace MikeSchweitzer.Git.Editor
             var lfsLock = Locks.First(lfsLock => lfsLock._Id == id);
             if (lfsLock._IsPending)
                 return;
-            
+
             lfsLock._IsPending = true;
             LockStatusChanged?.Invoke(lfsLock);
-            
+
             var task = Task.Run(() => InvokeLfs($"unlock --id {id}" + (force ? " --force" : "")));
             _tasks[task.Id] = task;
 
             RefreshLocksImpl(task);
         }
-        
+
         private void RefreshLocksImpl(Task<ProcessResult> priorTask = null)
         {
             Task<ProcessResult> task;
@@ -353,12 +353,12 @@ namespace MikeSchweitzer.Git.Editor
                     // if the priorTask had errors, then stop
                     if (t.Result.ErrorLines.Count > 0)
                         return null;
-                    
+
                     // if other tasks are in flight, then listing locks is a waste
                     // we only need to list locks after the last task is done
                     if (_tasks.Count > 0)
                         return null;
-                    
+
                     return InvokeLfs("locks");
                 });
             }
@@ -383,7 +383,7 @@ namespace MikeSchweitzer.Git.Editor
             else
             {
                 _stopAutoRefreshLocks = false;
-                
+
                 foreach (var line in result.OutLines)
                 {
                     var match = LocksResultRegex.Match(line);
@@ -396,12 +396,12 @@ namespace MikeSchweitzer.Git.Editor
                     lfsLock._AssetGuid = AssetDatabase.AssetPathToGUID(lfsLock._Path);
                     _Locks.Add(lfsLock);
                 }
-                
+
                 SortLocksImpl();
             }
 
             Save();
-            
+
             LocksRefreshed?.Invoke();
         }
 
@@ -449,16 +449,16 @@ namespace MikeSchweitzer.Git.Editor
                     var yNextSeparator = FindNextPathSeparator(y, i);
                     var xIsFolder = x[xNextSeparator] is '/' or '\\';
                     var yIsFolder = y[yNextSeparator] is '/' or '\\';
-                    
+
                     if (xIsFolder && yIsFolder)
                     {
                         // maybe a Unity bug...
-                        // 
+                        //
                         // 2 folders has a weird special case where NaturalCompare is ignored:
                         // Foo
                         // Foo 1
                         // Foo 10
-                        // 
+                        //
                         // NaturalCompare does this (this is also how files sort):
                         // Foo 1
                         // Foo 10
@@ -468,7 +468,7 @@ namespace MikeSchweitzer.Git.Editor
                         if (y[i] is '/' or '\\')
                             return 1;
                     }
-                    
+
                     if (xIsFolder && !yIsFolder)
                         return -1;
                     if (yIsFolder && !xIsFolder)
@@ -578,12 +578,12 @@ namespace MikeSchweitzer.Git.Editor
         {
             if (!string.IsNullOrWhiteSpace(_LfsProcessName))
                 return;
-            
+
             // On Windows, running lfs is our best bet to check if it exists
             if (Application.platform == RuntimePlatform.WindowsEditor)
             {
                 _LfsProcessName = "git-lfs";
-                
+
                 using var process = new Process();
                 process.StartInfo = new ProcessStartInfo(_LfsProcessName, "version")
                 {
@@ -605,14 +605,14 @@ namespace MikeSchweitzer.Git.Editor
                 }
                 return;
             }
-            
+
             // On Mac, Homebrew installs packages to a different folder on ARM (M series) vs Intel.
             // Homebrew also asks you to inject path vars into your shell profile, which we don't
             // have access to when we run Process.Start, since we want to redirect stdout.
             //
             // The hacky thing done here is guessing where your LFS is installed by checking both
             // paths. Invoking "command -v git-lfs" is a less-hacky-but-less-performant option.
-            // 
+            //
             // More on Homebrew diffs between Intel/ARM: https://apple.stackexchange.com/a/93179
             // More on Process.Start paths in C#: https://stackoverflow.com/a/41318134
             _LfsProcessName = "/opt/homebrew/bin/git-lfs";
